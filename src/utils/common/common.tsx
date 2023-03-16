095cfe2c042b10b3d50f52ffc3d6ada665254a6d
laremehpe <djatm108@outlook.com>
2023-03-17 00:11:42 +0800

    ideas

--- src/utils/common/common.tsx
+++ src/utils/common/common.tsx
@@ -1,3 +1,7 @@
 export function goto(wd: string) {
+  if (new RegExp(/^(http|https)/).test(wd)) {
+    window.open(wd);
+    return;
+  }
   window.location.href = `https://www.baidu.com/s?wd=${encodeURIComponent(wd)}`;
 }
--- src/views/Functions/Functions.jsx
+++ src/views/Functions/Functions.jsx
@@ -1,3 +1,25 @@
+import styles from "./Functions.module.css";
+const funList = [
+  //   {
+  //     name: "memo",
+  //   },
+  {
+    name: "favourite",
+  },
+  {
+    name: "js Lab",
+  },
+  {
+    name: "data sync textarea",
+  },
+];
+
 export function Functions() {
-  return <div>Functions</div>;
+  return (
+    <div className="box">
+      {funList.map((val) => (
+        <div className="item">{val.name}</div>
+      ))}
+    </div>
+  );
 }
