53b6d2d97dc635e696c6134e050c0eccbb7ec08a
djatm108@outlook.com <djatm108@outlook.com>
2023-03-25 10:00:08 +0800

    test

--- src/utils/common/common.tsx
+++ src/utils/common/common.tsx
@@ -3,6 +3,12 @@ export function goto(wd: string) {
     window.open(wd);
     return;
   }
+  if (new RegExp(/^(baidu )/).test(wd)) {
+    window.location.href =
+      "https://www.baidu.com/s?wd=" +
+      encodeURIComponent(wd.replace("baidu ", ""));
+    return;
+  }
   window.location.href =
     localStorage.getItem("searchEngine") + encodeURIComponent(wd);
 }
