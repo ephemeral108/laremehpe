ddd0f1f9237d115285e80f3abfd29535e69aad07
djatm108@outlook.com <djatm108@outlook.com>
2023-03-26 14:56:02 +0800

    fix search engine in chinese character situation

--- src/utils/common/common.tsx
+++ src/utils/common/common.tsx
@@ -3,12 +3,13 @@ export function goto(wd: string) {
     window.open(wd);
     return;
   }
-  if (new RegExp(/^(baidu )/).test(wd)) {
+  if (new RegExp(/^(baidu )|[\u2E80-\uFE4F]/).test(wd)) {
     window.location.href =
       "https://www.baidu.com/s?wd=" +
       encodeURIComponent(wd.replace("baidu ", ""));
     return;
   }
+
   window.location.href =
     localStorage.getItem("searchEngine") + encodeURIComponent(wd);
 }
