ae07b38b6de1bd70349461adb132281fe67871e1
djatm108@outlook.com <djatm108@outlook.com>
2023-04-04 11:06:55 +0800

    add 'and' mark

--- src/utils/common/common.tsx
+++ src/utils/common/common.tsx
@@ -3,7 +3,7 @@ const command: Array<{ cmd: RegExp; handler: (val: string) => void }> = [
     cmd: /(http|https):\/\//,
     handler(val) {
       let url: RegExpExecArray | null = new RegExp(
-        /http:\/\/[a-z\.\/%0-9A-Z#]*/
+        /http:\/\/[a-z\.\/%0-9A-Z#&]*/
       ).exec(val);
       if (url) window.open(url[0]);
       else encryptAndForward("baidu", val);
