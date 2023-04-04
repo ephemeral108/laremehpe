534420e6803a44600b6afb910d2cced5282af837
djatm108@outlook.com <djatm108@outlook.com>
2023-04-04 11:03:20 +0800

    automate

--- src/utils/common/common.tsx
+++ src/utils/common/common.tsx
@@ -1,13 +1,48 @@
+const command: Array<{ cmd: RegExp; handler: (val: string) => void }> = [
+  {
+    cmd: /^(baidu )/,
+    handler(val) {
+      encryptAndForward("baidu", val);
+    },
+  },
+  {
+    cmd: /^(google )/,
+    handler(val) {
+      encryptAndForward("google", val);
+    },
+  },
+  {
+    cmd: /(http|https):\/\//,
+    handler(val) {
+      let url: RegExpExecArray | null = new RegExp(
+        /http:\/\/[a-z\.\/%0-9A-Z#]*/
+      ).exec(val);
+      if (url) window.open(url[0]);
+      else encryptAndForward("baidu", val);
+    },
+  },
+  {
+    cmd: /[\u2E80-\uFE4F]/,
+    handler(val) {
+      encryptAndForward("baidu", val);
+    },
+  },
+];
+
+function encryptAndForward(engine: string, val: string) {
+  window.location.href =
+    (engine === "baidu"
+      ? "https://www.baidu.com/s?wd="
+      : "https://www.google.com/search?q=") +
+    encodeURIComponent(val.replace(engine + " ", ""));
+}
+
 export function goto(wd: string) {
-  if (new RegExp(/^(http|https)/).test(wd)) {
-    window.open(wd);
-    return;
-  }
-  if (new RegExp(/^(baidu )|[\u2E80-\uFE4F]/).test(wd)) {
-    window.location.href =
-      "https://www.baidu.com/s?wd=" +
-      encodeURIComponent(wd.replace("baidu ", ""));
-    return;
+  for (let i = 0, len = command.length; i < len; i++) {
+    if (new RegExp(command[i].cmd).test(wd)) {
+      command[i].handler(wd);
+      return;
+    }
   }
 
   window.location.href =
