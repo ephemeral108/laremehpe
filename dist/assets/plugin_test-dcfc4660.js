704b84cd9aa6361bb3f5fd450aa28997675eebb3
djatm108@outlook.com <djatm108@outlook.com>
2023-09-18 10:31:59 +0800

    forget build

---
+++ dist/assets/plugin_test-dcfc4660.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-da1d96ba.js";function l(s){t("test")}export{l as install};
--- dist/assets/plugin_test-e6986979.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-73d8be69.js";function l(s){t("test")}export{l as install};
